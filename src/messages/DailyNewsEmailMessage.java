74f4187ef6b07dde76f1e3306a573c938512095d
Ahmed Ashraf Ahmed <89888246+ahmeddashraff@users.noreply.github.com>
2022-05-27 23:07:50 +0200

    override annotation added

--- src/messages/DailyNewsEmailMessage.java
+++ src/messages/DailyNewsEmailMessage.java
@@ -1,7 +1,7 @@
 package messages;
 
 public class DailyNewsEmailMessage implements DailyNews{
-	
+	@Override
 	public String prepareMessage(String []placeHolders) {
 		// code to replace place holders of this type
 		return "";
--- src/messages/GradesAnnouncementEmailMessage.java
+++ src/messages/GradesAnnouncementEmailMessage.java
@@ -1,12 +1,12 @@
 package messages;
 
 public class GradesAnnouncementEmailMessage implements GradesAnnouncement{
-
+	@Override
 	public String prepareMessage(String[]placeHolders) {
 		// code to replace place holders of this type
 		return "";
 	}
-	
+	@Override
 	public boolean verifyGrades() {
 		// code to verify Grades before announcement
 		
