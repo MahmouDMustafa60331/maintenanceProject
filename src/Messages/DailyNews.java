3e71350107bf0fa5c717b9c88695cb89fecb4092
Ahmed Ashraf Ahmed <89888246+ahmeddashraff@users.noreply.github.com>
2022-05-27 19:27:22 +0200

    abstract factory added

---
+++ src/Messages/DailyNews.java
@@ -0,0 +1,5 @@
+package Messages;
+
+public interface DailyNews {
+    public String prepareMessage(String placeHolders[]);
+}
---
+++ src/Messages/TaskAdded.java
@@ -0,0 +1,6 @@
+package Messages;
+
+public interface TaskAdded {
+    public String prepareMessage(String placeholders[]);
+    public void addTeamDescription();
+}
